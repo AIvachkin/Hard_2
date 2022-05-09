21cced98d0fb2027b3af86729b76fb40686254e0
Андрей <andrews.ru83@gmail.com>
2022-05-09 21:29:28 +0300

    5.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int [][] matrix = new int[3][3] ;
+       matrix[0][0] = 1 ;
+       matrix[0][2] = 1 ;
+       matrix[1][1] = 1 ;
+       matrix[2][0] = 1 ;
+       matrix[2][2] = 1 ;
+        for (int[] row : matrix) {
+            for (int column : row) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
     }
 }
