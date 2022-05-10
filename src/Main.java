7e370683ce63c68b23ae13885c40208ac5e30d6a
Андрей <andrews.ru83@gmail.com>
2022-05-10 12:10:36 +0300

    5.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arr1 = {4,6,2,3,9,1} ;
+        float mediana = arr1.length / 2f;
+        for (int i = 0; i < arr1.length; i++) {
+            if (i < mediana) {
+            int temp = arr1[i];
+            arr1 [i] = arr1[arr1.length - 1 - i] ;
+            arr1 [arr1.length - 1 -i] = temp ;
+            System.out.print(arr1 [i] + " ");
+        } else if (i == arr1.length - 1) {
+                System.out.print(arr1 [i]) ; }
+            else System.out.print(arr1 [i] + " ");
+            }
     }
 }
