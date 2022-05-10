4be71bdad189a6237516b46b6956824f311af057
Андрей <andrews.ru83@gmail.com>
2022-05-10 11:35:53 +0300

    5.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        int [] arr1 = {5,6,3,9,1} ;
+        int [] arr2 = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr2 [i] = arr1[arr1.length - 1 - i] ;
+        }
+        System.out.println(Arrays.toString(arr1));
+        System.out.print(Arrays.toString(arr2));
+    }}
