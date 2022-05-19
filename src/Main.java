e1b690b14a705b1860fb40196b3bb328e1268880
Андрей <andrews.ru83@gmail.com>
2022-05-19 22:32:31 +0300

    Character.toUpperCase(c)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String fullName = "petrov Ivan ivanovich";
+        int a = fullName.indexOf(" ");
+        int b = fullName.indexOf(" ", a + 1);
+        String lastName = fullName.substring(0, a);
+        String firstName = fullName.substring(a + 1, b);
+        String middleName = fullName.substring(b + 1);
+        char[] charLast = lastName.toCharArray();
+        char[] charFirst = firstName.toCharArray();
+        char[] charMiddle = middleName.toCharArray();
+        charLast[0] = Character.toUpperCase(charLast[0]);
+        charFirst[0] = Character.toUpperCase(charFirst[0]);
+        charMiddle[0] = Character.toUpperCase(charMiddle[0]);
+        String fullNameNew = String.copyValueOf(charLast, 0, charLast.length) + " "
+                + String.copyValueOf(charFirst, 0, charFirst.length) + " " +
+                String.copyValueOf(charMiddle, 0, charMiddle.length);
+        System.out.println("fullNameNew = " + fullNameNew);
+
     }
 }
