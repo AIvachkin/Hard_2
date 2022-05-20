3f24291f2bdc4674b72f64547f401255451c133b
Андрей <andrews.ru83@gmail.com>
2022-05-20 13:43:04 +0300

    StringBuilder

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String first = "Ade" ;
+        String second = "nrw" ;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < first.length(); i++) {
+            result.append(first.charAt(i)) ;
+            result.append(second.charAt(i)) ;
+        }
+        System.out.println(result);
     }
 }
