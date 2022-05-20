3084c37cf1329d074ea2ec47f7c3f62ccb2fbd09
Андрей <andrews.ru83@gmail.com>
2022-05-20 14:15:43 +0300

    StringBuilder + append + delete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String str = "aabccddefgghiijjkk" ;
+//       StringBuilder result = new StringBuilder(str) ;
+       StringBuilder result2 = new StringBuilder() ;
+
+        for (int i = 0; i < str.length()-1 ; i++) {
+//            if (str.charAt(i) == str.charAt(i+1)) {
+//                result.deleteCharAt(i) ;
+//            }
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                result2.append(str.charAt(i)) ;
+            }
+        }
+//        System.out.println("result = " + result);
+        System.out.println("result2 = " + result2);
     }
 }
